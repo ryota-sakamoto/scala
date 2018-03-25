6c2fe5a5962cfea00aebc297d2043582b26d176d
ryota.sakamoto <ryota.sakamoto@aijus.com>
2018-03-26 02:10:35 +0900

    move file

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,10 @@
+mod scala;
+
 use std::num::ParseIntError;
-
-#[derive(Debug)]
-enum Either<E, R> {
-    Right(R),
-    Left(E),
-}
-
-trait EitherMonad<T, E, R> {
-    fn map(self, f: Box<Fn(R) -> T>) -> Either<E, T>;
-    fn flat_map(self, f: Box<Fn(R) -> Either<E, T>>) -> Either<E, T>;
-}
-
-impl<T, E, R> EitherMonad<T, E, R> for Either<E, R> {
-    fn map(self, f: Box<Fn(R) -> T>) -> Either<E, T> {
-        match self {
-            Either::Right(r) => Either::Right(f(r)),
-            Either::Left(e) => Either::Left(e),
-        }
-    }
-
-    // TODO
-    fn flat_map(self, f: Box<Fn(R) -> Either<E, T>>) -> Either<E, T> {
-        match self {
-            Either::Right(r) => f(r),
-            Either::Left(e) => Either::Left(e),
-        }
-    }
-}
-
-impl<E, R> Either<E, R> {
-    fn is_right(&self) -> bool {
-        match *self {
-            Either::Right(_) => true,
-            Either::Left(_) => false,
-        }
-    }
-
-    fn is_left(&self) -> bool {
-        match *self {
-            Either::Right(_) => false,
-            Either::Left(_) => true,
-        }
-    }
-
-    fn contains(self, f: Box<Fn(R) -> bool>) -> bool {
-        match self {
-            Either::Right(r) => f(r),
-            Either::Left(_) => false,
-        }
-    }
-
-    fn to_option(self) -> Option<R> {
-        match self {
-            Either::Right(r) => Some(r),
-            Either::Left(_) => None,
-        }
-    }
-}
+use scala::util::Either;
 
 fn main() {
+    println!("{:?}", f("100"));
 }
 
 fn f(s: &str) -> Either<ParseIntError, u64> {
