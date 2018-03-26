f1195062617126c848eae7c46b31b2a54f202e1b
ryota.sakamoto <ryota.sakamoto@aijus.com>
2018-03-27 01:57:25 +0900

    fix

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,66 +1,97 @@
+use std::cmp::PartialEq;
+
 #[derive(Debug)]
-pub enum Either<E, R> {
-    Right(R),
-    Left(E),
+pub enum Either<A, B> {
+    Left(A),
+    Right(B),
 }
 
-pub trait EitherMonad<T, E, R> {
-    fn map(self, f: Box<Fn(R) -> T>) -> Either<E, T>;
-    fn flat_map(self, f: Box<Fn(R) -> Either<E, T>>) -> Either<E, T>;
-}
-
-impl<T, E, R> EitherMonad<T, E, R> for Either<E, R> {
-    fn map(self, f: Box<Fn(R) -> T>) -> Either<E, T> {
+impl<A, B> Either<A, B> {
+    pub fn swap(self) -> Either<B, A> {
         match self {
-            Either::Right(r) => Either::Right(f(r)),
-            Either::Left(e) => Either::Left(e),
+            Either::Left(a) => Either::Right(a),
+            Either::Right(b) => Either::Left(b),
         }
     }
 
     // TODO test
-    fn flat_map(self, f: Box<Fn(R) -> Either<E, T>>) -> Either<E, T> {
+    pub fn for_each<U>(self, f: Box<Fn(B) -> U>) {
         match self {
-            Either::Right(r) => f(r),
-            Either::Left(e) => Either::Left(e),
+            Either::Right(b) => {
+                f(b);
+            }
+            _ => {}
         }
     }
-}
 
-impl<E, R> Either<E, R> {
     pub fn is_right(&self) -> bool {
-        match *self {
-            Either::Right(_) => true,
-            Either::Left(_) => false,
+        match self {
+            &Either::Left(_) => false,
+            &Either::Right(_) => true,
         }
     }
 
     pub fn is_left(&self) -> bool {
-        match *self {
-            Either::Right(_) => false,
-            Either::Left(_) => true,
+        match self {
+            &Either::Left(_) => true,
+            &Either::Right(_) => false,
         }
     }
 
     // TODO test
-    pub fn contains(self, f: Box<Fn(R) -> bool>) -> bool {
+    pub fn contains(self, f: Box<Fn(B) -> bool>) -> bool {
         match self {
-            Either::Right(r) => f(r),
-            Either::Left(_) => false,
+            Either::Right(b) => f(b),
+            _ => false,
         }
     }
 
-    pub fn get_or_else(self, f: R) -> R {
+    pub fn get_or_else(self, f: B) -> B {
         match self {
-            Either::Right(r) => r,
-            Either::Left(_) => f,
+            Either::Right(b) => b,
+            _ => f,
+        }
+    }
+
+    pub fn map<B1>(self, f: Box<Fn(B) -> B1>) -> Either<A, B1> {
+        match self {
+            Either::Left(a) => Either::Left(a),
+            Either::Right(b) => Either::Right(f(b)),
         }
     }
 
     // TODO test
-    pub fn to_option(self) -> Option<R> {
+    // fn flat_map<A1, B1>(self, f: Box<Fn(B) -> Either<A1, B1>>) -> Either<A1, B1> {
+    //     match self {
+    //         Either::Right(r) => f(r),
+    //         Either::Left(e) => self as Either<A1, B1>,
+    //     }
+    // }
+
+    pub fn to_option(self) -> Option<B> {
         match self {
-            Either::Right(r) => Some(r),
-            Either::Left(_) => None,
+            Either::Right(b) => Some(b),
+            _ => None,
         }
+    }
+}
+
+impl<A, B> PartialEq for Either<A, B> where A: PartialEq, B: PartialEq {
+    fn eq(&self, other: &Either<A, B>) -> bool {
+        let mut result = false;
+        if self.is_right() == other.is_right() {
+            match (self, other) {
+                (&Either::Left(ref a), &Either::Left(ref a1)) => {
+                    result = a == a1;
+                },
+                (&Either::Right(ref b), &Either::Right(ref b1)) => {
+                    result = b == b1;
+                },
+                _ => {
+                    result = false;
+                }
+            }
+        }
+        result
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,15 @@
 extern crate scala;
 
-use scala::util::{Either, EitherMonad};
+use scala::util::Either;
+
+#[test]
+fn swap_test() {
+    let right = create_right().swap();
+    assert_eq!(right, Either::Left(0));
+
+    let left = create_left().swap();
+    assert_eq!(left, Either::Right("Left".to_string()));
+}
 
 #[test]
 fn is_right_test() {
@@ -32,6 +41,26 @@ fn get_or_else_test() {
 
     let left = create_left();
     assert_eq!(left.get_or_else(1), 1);
+}
+
+#[test]
+fn to_option_test() {
+    let right = create_right().to_option();
+    assert_eq!(right, Some(0u64));
+
+    let left = create_left().to_option();
+    assert_eq!(left, None);
+}
+
+#[test]
+fn eq_test() {
+    let right = create_right();
+    assert!(right == Either::Right(0));
+    assert!(right != Either::Right(1));  
+
+    let left = create_left();
+    assert!(left == Either::Left("Left".to_string()));  
+    assert!(left != Either::Left("Right".to_string()));
 }
 
 fn create_right() -> Either<(), u64> {
